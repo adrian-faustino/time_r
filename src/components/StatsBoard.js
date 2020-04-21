caa514fca9b0a56f727cc40857c4968e37b12619
Adrian Faustino <adrianfaustino@Adrians-MacBook-Pro.local>
2020-04-21 06:09:26 -0400

    Added banner for total productive time

--- src/components/StatsBoard.js
+++ src/components/StatsBoard.js
@@ -7,37 +7,55 @@ import '../styles/StatsBoard.css';
 export default function StatsBoard(props) {
   const { h, m , s, ms } = formatMS(props.totalElapsed);
 
+  // fix this later after getting some sleep
+  // if (h) {
+  //   if (h === 1) {
+  //     output = ' 1 hour ';
+  //   } else {
+  //     output = `${h} hours `;
+  //   }
+  // } else if (m) {
+  //   if (h && m === 1) {
+  //     output += ' and one minute';
+  //   } else if (h && m) {
+  //     output += ` and ${m} minutes `;
+  //   } else if (m < 1 && s) {
+  //     output = ' a few seconds';
+  //   } else if (m === 1) {
+  //     output = ' a minute';
+  //   } else {
+  //     output = ` ${m} minutes`;
+  //   }
+  // } else if (s) {
+  //   if (s === 1) {
+  //     output = ' 1 second';
+  //   } else {
+  //     output = ` ${s} seconds`;
+  //   }
+  // }
+  const minutes = Math.floor(props.totalElapsed / 60000) // 60 * 1000;
   let output = '';
-  
-  if (h) {
-    if (h === 1) {
-      output = ' an hour ';
-    } else {
-      output = `${h} hours `;
-    }
-  } else if (m) {
-    if (h && m === 1) {
-      output += ' and one minute';
-    } else if (h && m) {
-      output += ` and ${m} minutes `;
-    } else if (m < 1 && s) {
-      output = ' a few seconds';
-    } else if (m === 1) {
-      output = ' a minute';
-    } else {
-      output = ` ${m} minutes`;
-    }
-  } else if (s) {
-    if (s === 1) {
-      output = ' 1 second';
-    } else {
-      output = ` ${s} seconds`;
-    }
+
+  if (minutes === 1) {
+    output += ` 1 minute`;
+  } else if (minutes > 1) {
+    output += ` ${minutes} minutes`;
+  }
+
+  if (s > 0 && minutes >= 1) {
+    output += ' and';
+  }
+
+  if (s === 1) {
+    output += ` 1 second`;
+  } else if (s > 1) {
+    output += ` ${s} seconds`;
   }
   
   return (
     <div className="stats-board__container">
-      <span className="stats-bard__banner">You've been productive for {output}</span>
+      <span className="stats-board__banner">{(s > 0 || minutes > 0) && `You've been productive for:`}</span>
+      <span className="stats-board__banner time">{props.totalElapsed ? output : ''}</span>
     </div>
   )
 }
